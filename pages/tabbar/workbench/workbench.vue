cd78d149131df61fddf7152444b6e58c1cd29fdc
LC-daoxin <457121827@qq.com>
2020-11-20 11:15:05 +0800

    add department-detail

--- pages/tabbar/workbench/workbench.vue
+++ pages/tabbar/workbench/workbench.vue
@@ -1,5 +1,6 @@
 <template>
 	<view class="mine">
+		<department-detail :info="info"></department-detail>
 		<member-management></member-management>
 		<workbench title='成员申请'></workbench>
 		<!-- <u-tabbar v-model="current" height="56px" :list="list" inactive-color="#7A7E83" active-color="#5098ff" :mid-button="true"></u-tabbar> -->
@@ -10,6 +11,9 @@
 	export default {
 		data() {
 			return {
+				info: {
+					departmentsName: '心血管内科'
+				}, // 科室信息
 				list: [],
 				current: 1
 			};
