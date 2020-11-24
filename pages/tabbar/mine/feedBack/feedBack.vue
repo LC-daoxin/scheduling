e11b7aaeb057f0f80f764a1652391c1b90c2fb84
Finn <xiongfei@deltaphone.com.cn>
2020-11-24 10:00:30 +0800

    添加消息、个人设置、反馈意见页面

---
+++ pages/tabbar/mine/feedBack/feedBack.vue
@@ -0,0 +1,79 @@
+<template>
+  <view class="feed-back">
+    <form @submit="formSubmit" @reset="formReset">
+      <view class="uni-form-item uni-column">
+        <view class="title">标题</view>
+        <input class="uni-input" name="input" placeholder="请输入反馈标题"/>
+      </view>
+      <view class="uni-form-item uni-column">
+        <view class="title">内容</view>
+        <view class="uni-textarea">
+          <textarea placeholder="请输入反馈内容"/>
+        </view>
+      </view>
+      <view class="uni-btn-v">
+        <button form-type="submit" type="primary">提交</button>
+        <button class="reset-btn" type="default" form-type="reset">重制</button>
+      </view>
+    </form>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    formSubmit: function (e) {
+      console.log('form发生了submit事件，携带数据为：' + JSON.stringify(e.detail.value))
+      var formdata = e.detail.value
+      uni.showModal({
+        content: '表单数据内容：' + JSON.stringify(formdata),
+        showCancel: false
+      });
+    },
+    formReset: function (e) {
+      console.log('清空数据')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.feed-back {
+  height: calc(100vh - 44px);
+  background-color: #f8f8f8;
+  padding: 1em 1em 0;
+}
+
+.feed-back .uni-btn-v uni-button+uni-button {
+  margin-top: 0.5em;
+}
+
+.feed-back .uni-btn-v .reset-btn {
+  background-color: #fff;
+}
+
+::v-deep button {
+  font-size: inherit;
+}
+
+.uni-form-item {
+  margin-bottom: 1em;
+}
+
+.uni-form-item .title {
+  font-size: 1.2em;
+  font-weight: bold;
+  padding: 20rpx 0;
+}
+
+.uni-form-item input,
+.uni-form-item .uni-textarea {
+  padding: 0.4em;
+  border: 1px solid #eee;
+  border-radius: 0.4em;
+  background-color: #fff;
+}
+</style>
--- pages/tabbar/notice/notice.vue
+++ pages/tabbar/notice/notice.vue
@@ -1,49 +1,46 @@
 <template>
 	<view class="wrap">
 		<u-notice-bar class="notice" mode="horizontal" :list="notice" :more-icon="true"></u-notice-bar>
-		<view class="uTabs">
-			<u-tabs-swiper ref="uTabs" :list="tabslist" :current="currentTabs" @change="tabsChange" :is-scroll="false"
-			 swiperWidth="750"></u-tabs-swiper>
-			 <u-line color="#dbdfdf"></u-line>
-		</view>
-		<swiper :current="swiperCurrent" @transition="transition" @animationfinish="animationfinish" :style="{height: clientHeight + 'px'}">
-			<swiper-item class="swiper-item" v-for="(tabsitem, index) in tabsContent" :key="index">
-				<scroll-view scroll-y @scrolltolower="onreachBottom">
-					<view :class="{'imgHead': true, 'no_read': !item.readStatus, 'read': item.readStatus}" v-for="(item, index) in tabsitem.msgList">
-						<view class="imgBox">
-							<view :style="{background:item.color}" v-if="item.userImgUrl === null" class="text">{{ item.userName }}</view>
-							<image v-else src="item.userImgUrl"></image>
-						</view>
-						<view class="contentBox">
-							<view class="contentBoxHead">
-								<view class="contentBoxHead_title">#{{ item.groupName }}</view>
-								<view class="contentBoxHead_time">{{ item.sendTime }}</view>
+		<view class="tab">
+			<view class="tab-header">
+				<view :class="`tab-header_item ${isActiveTab(0)}`" @click="toogleTab(0)"><p>全部</p></view>
+				<view :class="`tab-header_item ${isActiveTab(1)}`" @click="toogleTab(1)"><p>未读</p></view>
+				<view :class="`tab-header_item ${isActiveTab(2)}`" @click="toogleTab(2)"><p>待处理</p></view>
+			</view>
+			<view class="tab-inner">
+				<view class="tab-inner_item" v-for="(tabsitem, index) in tabsContent" :key="index" v-show="currentTabIndex === index">
+					<view class="swiper-item" @click="msgDetail()">
+						<view :class="{ imgHead: true, no_read: !item.readStatus, read: item.readStatus }" v-for="(item, index) in tabsitem.msgList">
+							<view class="imgBox">
+								<view :style="{ background: item.color }" v-if="item.userImgUrl === null" class="text">{{ item.userName }}</view>
+								<image v-else src="item.userImgUrl"></image>
 							</view>
-							<view class="contentBoxTitle">
-								{{ item.Title }}
-							</view>
-							<view class="contentBoxText">
-								{{ item.content }}
+							<view class="contentBox">
+								<view class="contentBoxHead">
+									<view class="contentBoxHead_title">#{{ item.groupName }}</view>
+									<view class="contentBoxHead_time">{{ item.sendTime }}</view>
+								</view>
+								<view class="contentBoxTitle">{{ item.Title }}</view>
+								<view class="contentBoxText">{{ item.content }}</view>
 							</view>
 						</view>
 					</view>
-				</scroll-view>
-			</swiper-item>
-		</swiper>
-		<!-- <u-tabbar v-model="current" height="56px" :list="list" inactive-color="#7A7E83" active-color="#5098ff" :mid-button="true"></u-tabbar> -->
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				list: [],
-				current: 3,
-				notice: [
-					'本月由于工程项目任务急项目截止时间紧，需要全体成员每天8点上班，晚上10点下班！'
-				],
-				tabsContent: [{
+export default {
+	data() {
+		return {
+			list: [],
+			currentTabIndex: 0,
+			current: 3,
+			notice: ['本月由于工程项目任务急项目截止时间紧，需要全体成员每天8点上班，晚上10点下班！'],
+			tabsContent: [
+				{
 					Type: '全部',
 					msgList: [
 						{
@@ -112,7 +109,8 @@
 							msgNumber: 12
 						}
 					]
-				}, {
+				},
+				{
 					Type: '已读',
 					msgList: [
 						{
@@ -129,7 +127,8 @@
 							msgNumber: 12
 						}
 					]
-				}, {
+				},
+				{
 					Type: '待处理',
 					msgList: [
 						{
@@ -159,119 +158,171 @@
 							msgNumber: 12
 						}
 					]
-				}],
-				tabslist: [{
+				}
+			],
+			tabslist: [
+				{
 					name: '全部'
-				}, {
+				},
+				{
 					name: '未读'
-				}, {
+				},
+				{
 					name: '待处理'
-				}],
-				currentTabs: 0,
-				swiperCurrent: 0,
-				clientHeight: 0
-			};
+				}
+			],
+			currentTabs: 0,
+			swiperCurrent: 0,
+			clientHeight: 0
+		};
+	},
+	mounted() {
+		this.list = this.$store.state.vuex_tabbar;
+	},
+	methods: {
+		msgDetail() {
+			uni.navigateTo({
+				url: '/pages/tabbar/notice/noticeDetail/noticeDetail'
+			})
 		},
-		mounted () {
-			this.list = this.$store.state.vuex_tabbar
+		toogleTab(index) {
+			this.currentTabIndex = index;
 		},
-		onReady () {
-			let that = this
-			const systemRes = uni.getSystemInfoSync();
-			let h1, h2;
-			uni.createSelectorQuery().in(this).select('.uTabs').boundingClientRect(result => {
-				h1 = result.height
-		　　 }).exec()
-			uni.createSelectorQuery().in(this).select('.notice').boundingClientRect(result => {
-				h2 = result.height
-				this.clientHeight = systemRes.screenHeight - h1 - h2
-		　　 }).exec()
+		isActiveTab(index) {
+			return this.currentTabIndex === index ? 'tab-header_item_active' : '';
 		},
-		methods: {
-			// tabs通知swiper切换
-			tabsChange(index) {
-				this.swiperCurrent = index;
-			},
-			// swiper-item左右移动，通知tabs的滑块跟随移动
-			transition(e) {
-				let dx = e.detail.dx;
-				this.$refs.uTabs.setDx(dx);
-			},
-			// 由于swiper的内部机制问题，快速切换swiper不会触发dx的连续变化，需要在结束时重置状态
-			// swiper滑动结束，分别设置tabs和swiper的状态
-			animationfinish(e) {
-				let current = e.detail.current;
-				this.$refs.uTabs.setFinishCurrent(current);
-				this.swiperCurrent = current;
-				this.current = current;
-			},
-			// scroll-view到底部加载更多
-			onreachBottom() {
-			}
-		}
+		// tabs通知swiper切换
+		tabsChange(index) {
+			this.swiperCurrent = index;
+		},
+		// swiper-item左右移动，通知tabs的滑块跟随移动
+		transition(e) {
+			let dx = e.detail.dx;
+			this.$refs.uTabs.setDx(dx);
+		},
+		// 由于swiper的内部机制问题，快速切换swiper不会触发dx的连续变化，需要在结束时重置状态
+		// swiper滑动结束，分别设置tabs和swiper的状态
+		animationfinish(e) {
+			let current = e.detail.current;
+			this.$refs.uTabs.setFinishCurrent(current);
+			this.swiperCurrent = current;
+			this.current = current;
+		},
+		// scroll-view到底部加载更多
+		onreachBottom() {}
 	}
+};
 </script>
 
 <style lang="scss">
-	.wrap {
-		height: calc(100vh - var(--window-top));
-		.swiper-item {
-			height: 500px;
-			.no_read {
-				color: #333;
+.wrap {
+	height: calc(100vh - var(--window-top) - 50px);
+
+	.tab {
+		.tab-header {
+			display: flex;
+			border-bottom: 1px solid #e6e6e6;
+
+			.tab-header_item {
+				flex: 1;
+				font-size: 0.8rem;
+				color: rgb(48, 49, 51);
+				padding: 0.5em 0 0.6em 0;
+				text-align: center;
+				cursor: pointer;
 			}
-			.read {
-				color: #ccc;
+			.tab-header_item_active {
+				position: relative;
+				color: rgb(41, 121, 255);
+				font-weight: bold;
 			}
-			.imgHead {
+			.tab-header_item_active:after {
+				content: '';
+				position: absolute;
+				left: 50%;
+				bottom: 0.1em;
+				width: 1.5em;
+				height: 0.2em;
+				border-radius: 2em;
+				background-color: rgb(41, 121, 255);
+				transform: translateX(-50%);
+			}
+		}
+		.tab-inner {
+			display: flex;
+			height: calc(100vh - 12em);
+			overflow: hidden auto;
+
+			.tab-inner_item {
+				width: 100vw;
+			}
+		}
+	}
+
+	.swiper-item {
+		.no_read {
+			color: #333;
+		}
+
+		.read {
+			color: #ccc;
+		}
+
+		.imgHead {
+			display: flex;
+			align-items: center;
+			flex-direction: row;
+
+			.imgBox {
+				width: 75px;
+				height: 70px;
 				display: flex;
+				justify-content: center;
 				align-items: center;
-				flex-direction: row;
-				.imgBox {
-					width: 75px;
-					height: 70px;
+
+				.text {
+					width: 40px;
+					height: 40px;
+					border-radius: 50%;
+					color: #fff;
+					font-weight: 500;
 					display: flex;
 					justify-content: center;
 					align-items: center;
-					.text {
-						width: 40px;
-						height: 40px;
-						border-radius: 50%;
-						color: #FFF;
-						font-weight: 500;
-						display: flex;
-						justify-content: center;
-						align-items: center;
-					}
 				}
-				.contentBox {
+			}
+
+			.contentBox {
+				display: flex;
+				flex-direction: column;
+				box-sizing: border-box;
+				padding: 8px 10px 0;
+				height: 70px;
+				width: calc(100% - 75px);
+				border-bottom: 1px solid #dbdfdf;
+
+				.contentBoxHead {
 					display: flex;
-					flex-direction: column;
-					box-sizing: border-box;
-					padding: 8px 10px 0;
-					height: 70px;
-					width: calc(100% - 75px);
-					border-bottom: 1px solid #dbdfdf;
-					.contentBoxHead {
-						display: flex;
-						align-items: center;
-						flex-direction: row;
-						justify-content: space-between;
-						width: 100%;
-						font-size: 12px;
-					}
-					.contentBoxTitle {
-						font-size: 14px;
-					}
-					.contentBoxText {
-						font-size: 12px;
-						overflow: hidden;    
-						text-overflow:ellipsis;    
-						white-space: nowrap;
-						width: 90%;
-					}
+					align-items: center;
+					flex-direction: row;
+					justify-content: space-between;
+					width: 100%;
+					font-size: 12px;
+				}
+
+				.contentBoxTitle {
+					font-size: 14px;
+				}
+
+				.contentBoxText {
+					font-size: 12px;
+					overflow: hidden;
+					text-overflow: ellipsis;
+					white-space: nowrap;
+					width: 90%;
 				}
 			}
 		}
 	}
+}
 </style>
